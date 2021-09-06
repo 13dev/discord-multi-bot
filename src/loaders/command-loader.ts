fb8a9212852ab752105ac95c25b17086859fcc76
Leo Oliveira <leooliveira@7graus.com>
2021-09-07 00:26:10 +0100

    Loaders: - Add missing comments
    Signed-off-by: Leo Oliveira <leooliveira@7graus.com>

--- src/loaders/command-loader.ts
+++ src/loaders/command-loader.ts
@@ -2,14 +2,18 @@ import {Command} from '@src/command'
 import {Container} from 'typeorm-typedi-extensions'
 import {Logger} from '@utils/logger'
 import CommandResolver from '@src/resolver/command-resolver'
-
 const getFiles = require('node-recursive-directory')
 
+/**
+ * Loads all classes in 'commands' directory, works with only one level deep.
+ */
 export default async function commandLoader() {
     (await getFiles('src/commands', true)).map(async (file: any) => {
-        const dir = file.dirname === 'commands' ? '' : file.dirname + '/'
-        const className = await import('@commands/' + dir + file.filename.replace('.ts', ''))
 
+        // Ignore the root directory
+        const dir = file.dirname === 'commands' ? '' : file.dirname + '/'
+
+        const className = await import('@commands/' + dir + file.filename.replace('.ts', ''))
 
         const commandInstance = Container.get<Command>(className.default)
         const commandKey = commandInstance.commandOptions.group + '_' + commandInstance.commandOptions.name
