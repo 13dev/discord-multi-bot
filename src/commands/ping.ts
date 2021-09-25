d3809d84e9309a441f27e805523e50c928fad7d3
Leo Oliveira <leo@13dev.pt>
2021-09-25 16:53:00 +0100

    fix(core): add package-lock
    Signed-off-by: Leo Oliveira <leo@13dev.pt>

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discord.js'
-import { Command, CommandOptions } from "@src/command";
+import { Command, CommandOptions } from '@src/command'
 import DiscordClient from '@src/discord-client'
 import { Container, Service } from 'typedi'
 import { LOTTERY_ID } from '@utils/consts'
@@ -10,7 +10,7 @@ export default class extends Command {
         return {
             name: 'ping',
             signature: {
-              command: 'ping'
+                command: 'ping',
             },
             description: 'Pings the bot.',
             category: 'Information',
@@ -19,12 +19,13 @@ export default class extends Command {
     }
 
     public async run(message: Message): Promise<void> {
-        const m = await message.reply('Ping?')
-        await m.edit(
-            ':ping_pong: Pong! \n Latency is ' +
-                (m.createdTimestamp - message.createdTimestamp) +
-                'ms. \n API Latency is ' +
-                Math.round(this.client.ws.ping)
+        const sendedMessage = await message.reply('Hello!')
+        const latency =
+            sendedMessage.createdTimestamp - message.createdTimestamp
+        const ping = Math.round(this.client.ws.ping)
+
+        await sendedMessage.edit(
+            `:ping_pong: Pong! \n - Latency is ${latency} ms. \n API Latency is ${ping}`
         )
     }
 }
