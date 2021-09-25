92cc4ddff3f42e1d00edad7afec6e21bd946c842
Leo Oliveira <leo@13dev.pt>
2021-09-25 16:03:57 +0100

    fix(commands): Finish clear command
    Signed-off-by: Leo Oliveira <leo@13dev.pt>

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -2,6 +2,7 @@ import { DMChannel, Message } from 'discord.js'
 import { Command } from '@src/command'
 import DiscordClient from '@src/discord-client'
 import { Service } from 'typedi'
+import { Logger } from '@utils/logger'
 
 @Service()
 export default class extends Command {
@@ -18,11 +19,16 @@ export default class extends Command {
         if (message.channel instanceof DMChannel) {
             return
         }
+        const limit = Number(args[0]) + 1
 
-        message.channel
-            .bulkDelete(parseInt(args[0]))
-            .catch((error) =>
-                message.reply(`Erro a eliminar mensagens: ${error}`)
-            )
+        if (limit < 0 || limit > 20) {
+            await message.reply('Introduz numbero entre 0 - 20.')
+            return
+        }
+
+        message.channel.bulkDelete(limit).catch((error) => {
+            Logger.info('Error deleting messages', { error: error })
+            message.reply(`Erro a eliminar mensagens.`)
+        })
     }
 }
--- src/events/on-message-event.ts
+++ src/events/on-message-event.ts
@@ -1,37 +1,46 @@
-import {BotEvent, EventType} from '@events/index'
+import { BotEvent, EventType } from '@events/index'
 import DiscordClient from '@src/discord-client'
-import {Container as Container1} from 'typedi'
+import { Container as Container1 } from 'typedi'
 import CommandResolver from '@src/resolver/command-resolver'
 import UserResolver from '@src/resolver/user-resolver'
-import {USER} from '@utils/consts'
-import {DiscordLotteryError} from '@src/errors'
-import {Logger} from '@utils/logger'
+import { USER } from '@utils/consts'
+import { DiscordLotteryError } from '@src/errors'
+import { Logger } from '@utils/logger'
 
 export default class OnReadyEvent implements BotEvent {
     type: EventType = EventType.MESSAGE
 
-    constructor(private client: DiscordClient) {
-    }
+    constructor(private client: DiscordClient) {}
 
     public async run(args: any): Promise<void> {
         const [message] = args
 
-        if (message.author.bot || !message.content.startsWith(this.client.config.prefix)) return
+        if (
+            message.author.bot ||
+            !message.content.startsWith(this.client.config.prefix)
+        )
+            return
 
         const argus = message.content.split(/\s+/g)
         const command = argus.shift()!.slice(this.client.config.prefix.length)
-        const commandGroup = argus.shift() || ''
 
-        const commandClass = CommandResolver.resolve(commandGroup + '_' + command)
+        console.log(argus, command)
+
+        const commandClass = CommandResolver.resolve(command)
 
         if (!commandClass) {
             return
         }
 
-        Logger.info('Calling command class: ' +  commandClass.constructor.name + ' - arguments: ' + argus)
-        await UserResolver
-            .resolve(message.author)
-            .then(user => Container1.set(USER, user))
+        Logger.info(
+            'Calling command class: ' +
+                commandClass.constructor.name +
+                ' - arguments: ' +
+                argus
+        )
+        await UserResolver.resolve(message.author).then((user) =>
+            Container1.set(USER, user)
+        )
 
         //Verify if the user can actually run this command
         if (!(await commandClass.canRun(message.author, message))) return
@@ -39,7 +48,6 @@ export default class OnReadyEvent implements BotEvent {
         try {
             await commandClass.run(message, argus)
         } catch (error) {
-
             //We will use the Error message prop to give user feedback what gone wrong.
             if (error instanceof DiscordLotteryError) {
                 message.reply(error.message)
@@ -48,6 +56,5 @@ export default class OnReadyEvent implements BotEvent {
 
             throw error
         }
-
     }
 }
